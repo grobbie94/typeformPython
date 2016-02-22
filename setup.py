e62f320b6dd721b8675196a4b210fda8211e54b5
Robert Banks <robertedwardbanks@gmail.com>
2016-02-22 16:39:23 +0800

    Admin - Added __init__ files and finished setup.py and license

---
+++ setup.py
@@ -0,0 +1,11 @@
+setup(name='typeformPython',
+      version='0.1',
+      description='A Python Wrapper for the Typeform API',
+      url='http://github.com/grobbie94/typeformPython',
+      author='Robert Banks',
+      license='MIT',
+      packages=['typeformPython'],
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose','unittest'],
+      install_requires=['requests'])
