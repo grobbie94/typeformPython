af5987d2892eb05ae1ae22976a5eda18497e45a2
Robert Banks <robertedwardbanks@gmail.com>
2016-02-22 16:14:36 +0800

    Wrote base typeform class

---
+++ typeformPython/typeform.py
@@ -0,0 +1,17 @@
+'''
+Author: Robert Banks
+This class connects to typeform and is used to return formatted typeform
+responses from the typeform API.
+'''
+
+import requests
+
+class typeform:
+
+    def __init__(self, API_KEY):
+        self.API_KEY = API_KEY
+
+    def getForm (self, formKey):
+        apiAddress = "https://api.typeform.com/v0/form/%s?key=%s" % formKey, self.API_KEY
+        formJson = requests.get(apiAddress).json()
+        return form(formJson)
