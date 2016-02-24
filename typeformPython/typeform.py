266cefaa91d0da4b7b1279a9fd512a96c3436054
Robert Banks <robertedwardbanks@gmail.com>
2016-02-24 18:39:16 +0800

    Added network exception

--- typeformPython/typeform.py
+++ typeformPython/typeform.py
@@ -1,4 +1,5 @@
 import requests
+from exceptions import NetworkError
 
 class typeform:
     """
@@ -16,6 +17,21 @@ class typeform:
         Parameters: formKey - Check the typeform API docs for info
         """
         #TODO implement exception for no network etc.
-        apiAddress = "https://api.typeform.com/v0/form/%s?key=%s" % formKey, self.API_KEY
-        formJson = requests.get(apiAddress).json()
-        return form(formJson)
+        apiAddress = "https://api.typeform.com/v0/form/{0}?key={1}".format(formKey, self.API_KEY)
+        form = requests.get(apiAddress)
+        statusCode = form.status_code
+        redirectAddress = "https://api.typeform.com/login/"
+        if statusCode == 200 and form.url != redirectAddress:
+            formJson = form.json()
+            return form(formJson)
+        elif statusCode == 404:
+            try:
+                raise NetworkError("404 Not Found - Check Form Key")
+            except NetworkError, instance:
+                print "NetworkError: " + instance.par
+
+        else:
+            try:
+                raise NetworkError("NetworkError -  Check API_KEY")
+            except NetworkError, instance:
+                print "NetworkError: " + instance.par
